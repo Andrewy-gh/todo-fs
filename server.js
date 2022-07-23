42e69840602c7ed7586d8efd2a9888a21fb713df
Andy-git985 <andy.yu617@gmail.com>
2022-07-22 20:40:01 -0400

    dotenv and mongo connect

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const connectionString = process.env.DB_URL;
 
 app.set('view engine', 'ejs');
 
@@ -10,6 +11,20 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(connectionString, {
+  dbName: 'todo-list',
+  useNewUrlParser: true,
+});
+
+const db = mongoose.connection;
+db.once('open', (_) => {
+  console.log('Database connected');
+});
+
+db.on('error', (err) => {
+  console.error('connection error');
+});
 
 // Routes
 app.get('/', async (req, res) => {
